9b96352d0e49c77c343a14facf52210272a25d79
thaisperlho <carvalho.thais0301@gmail.com>
2022-12-15 18:05:48 -0300

    update PostGetRepository.java

--- falae/src/main/java/br/edu/fateccotia/falae/repository/PostGetRepository.java
+++ falae/src/main/java/br/edu/fateccotia/falae/repository/PostGetRepository.java
@@ -14,7 +14,7 @@ import br.edu.fateccotia.falae.model.PostsGet;
 @Repository
 public interface PostGetRepository extends JpaRepository<PostsGet, Integer> {
 	
-	@Query(value = "SELECT p.*, sum(CASE WHEN r.tipo_reaction  = 1 then 1 else 0 end) as gostei, sum(CASE WHEN r.tipo_reaction  = 2 then 1 else 0 end) as nao_gostei FROM posts as p left join reactions r on p.id = r.fk_post_id group by p.id, p.dt_post, p.post, p.fk_user_id, p.model_post", 
+	@Query(value = "SELECT p.*, sum(CASE WHEN r.tipo_reaction  = 1 then 1 else 0 end) as gostei, sum(CASE WHEN r.tipo_reaction  = 2 then 1 else 0 end) as nao_gostei FROM posts as p left join reactions r on p.id = r.fk_post_id group by p.id, p.dt_post, p.post, p.fk_user_id, p.model_post order by p.dt_post desc", 
 			nativeQuery = true)
 	List<PostsGet> buscaComQtdReaction();
 
